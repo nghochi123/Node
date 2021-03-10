1b48bb8b3a1cd9cbc2d8f4badc4d717af469c051
nghochi123 <nghochi123@gmail.com>
2021-03-10 22:33:43 +0800

    small stuff

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -132,6 +132,18 @@ app.listen(port, ()=>{
     console.log('Server is up on port ' + port);
 });
 
+const bcrypt = require('bcryptjs');
+
+const myFunction = async () => {
+    const password = 'Red12345!';
+    const hashedPassword = await bcrypt.hash(password, 8);
+
+    console.log(password);
+    console.log(hashedPassword);
+}
+
+myFunction()
+
 
 
 
