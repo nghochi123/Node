964c1fdc0d0beb6f8abd72a1b76ff985a05b60af
nghochi123 <nghochi123@gmail.com>
2021-04-25 23:02:33 +0800

    looking at how service workers work

---
+++ PWA/Service Workers/js/main.js
@@ -0,0 +1,9 @@
+//Make sure service workers are supported
+if ("serviceWorker" in navigator) {
+  window.addEventListener('load', ()=>{
+      navigator.serviceWorker
+      .register('sw_cached_pages.js')
+      .then(reg=>console.log('Service Worker: Registered'))
+      .catch(err=> console.log(`Service Worker: Error ${err}`))
+  })
+}
